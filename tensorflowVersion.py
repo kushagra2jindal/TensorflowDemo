9fbc80ab181aff88bb26d83bc002d5e30e459d8d
KUSHAGRA JINDAL <kushagra2jindal@gmail.com>
2020-07-01 18:44:37 +0530

    check tenserflow version

---
+++ tensorflowVersion.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  1 18:43:18 2020
+
+@author: kushagra
+"""
+
+
+import tensorflow as tf 
+
+print("TensorFlow version: ", tf.__version__)
