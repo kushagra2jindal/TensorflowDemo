48cc82badb59c712f721e819308f387890db8d04
KUSHAGRA JINDAL <kushagra2jindal@gmail.com>
2020-07-09 19:01:16 +0530

    model building for image classifier

--- imageClassifier.py
+++ imageClassifier.py
@@ -20,3 +20,14 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Dropout(0.2),
   tf.keras.layers.Dense(10)
 ])
+
+predictions = model(x_train[:1]).numpy()
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+loss_fn(y_train[:1], predictions).numpy()
+model.compile(optimizer='adam',loss=loss_fn,metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+
+model.evaluate(x_test,  y_test, verbose=2)
+
